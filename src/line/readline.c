672eda288fd172e0017fca6b0d1cad8f042ef45b
R0-main <justromixx@gmail.com>
2025-02-14 17:22:31 +0100

    added parsing pipeline

--- src/line/readline.c
+++ src/line/readline.c
@@ -6,11 +6,12 @@
 /*   By: rguigneb <rguigneb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 19:01:37 by romain            #+#    #+#             */
-/*   Updated: 2025/02/14 13:38:09 by rguigneb         ###   ########.fr       */
+/*   Updated: 2025/02/14 17:22:13 by rguigneb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "parsing.h"
 #include "paths.h"
 
 void	handle_readline(t_minishell *data)
@@ -21,19 +22,13 @@ void	handle_readline(t_minishell *data)
 	if (line && ft_strncmp(line, "exit", ft_strlen("exit")) == 0)
 		data->stop = true;
 	if (line && ft_strlen(line) > 0)
-	{
 		data->exit_code = 0;
-		ft_fprintf(1, RED);
-		ft_fprintf(1, line);
-		ft_fprintf(1, "\n");
-	}
 	else
 		data->exit_code = 1;
 	if (line)
 	{
 		add_history(line);
-		if (ft_strlen(line))
-			combine_paths(data, line, "../../");
+		parse_line(data, line);
 	}
 	if (!line)
 	{
