833e9aad4d1898d885566b5e6d93e61f80e1aa89
R0-main <justromixx@gmail.com>
2025-02-19 10:26:21 +0100

    fix exit segfault

--- src/execution/built_in/commands/exit.c
+++ src/execution/built_in/commands/exit.c
@@ -6,7 +6,7 @@
 /*   By: rguigneb <rguigneb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 14:33:52 by rguigneb          #+#    #+#             */
-/*   Updated: 2025/02/19 10:17:51 by rguigneb         ###   ########.fr       */
+/*   Updated: 2025/02/19 10:25:56 by rguigneb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,8 +43,8 @@ int	exit_command(t_minishell *data, t_command *command)
 			ft_fprintf(STDERR_FILENO, EXIT_NOT_A_NUMERIC_ARG, command->argv[1]);
 			safe_exit(EXIT_NOT_A_NUMERIC_ARG_CODE);
 		}
+		data->exit_code = (unsigned char)ft_atoi(command->argv[1]);
 	}
-	data->exit_code = (unsigned char)ft_atoi(command->argv[1]);
 	safe_exit(data->exit_code);
 	return (EXIT_SUCCESS);
 }
